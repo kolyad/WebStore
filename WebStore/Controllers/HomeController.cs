f95e5d1ee0e020a012a2a17580e321ba8fbdd58d
kolyad <kolyad@mail.ru>
2021-01-21 01:15:27 +0300

    Добавил контроллер, модели и представления для отображения списка сотрудников

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly List<Employee> _employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Александр", Patronymic = "Сергеевич", Age = 20 },
+            new Employee { Id = 2, LastName = "Васечкин", FirstName = "Иван", Patronymic = "Васильевич", Age = 35 },
+            new Employee { Id = 3, LastName = "Прохоров", FirstName = "Михаил", Patronymic = "Фёдорович", Age = 63 }
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_employees);
+        }
+    }
+}
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -3,10 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebStore
 {
@@ -16,6 +12,8 @@ namespace WebStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +28,9 @@ namespace WebStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{Id?}");
             });
         }
     }
